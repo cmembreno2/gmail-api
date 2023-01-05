7fa8229932daea119b453844b8919f8b6fc458fe
username <carlosmembreno2@hotmail.com>
2023-01-05 13:57:02 -0600

    enhancement

--- routes/getThreads.js
+++ routes/getThreads.js
@@ -1,13 +1,12 @@
 const express = require('express');
 require('dotenv').config();
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const {OAuth2Client} = require('google-auth-library');
+const http = require('http');
+const url = require('url');
+const open = require('open');
+const destroyer = require('server-destroy');
+const keys = require('../credentials.json');
 
 const router = express.Router();
 
@@ -17,48 +16,49 @@ router.get('/threads', async (req,res)=> {
     
     const results = [];
     
-    try{
-
-        async function loadSavedCredentialsIfExist() {
-            try {
-              const content = await fs.readFile(TOKEN_PATH);
-              const credentials = JSON.parse(content);
-              return google.auth.fromJSON(credentials);
-            } catch (err) {
-              return null;
-            }
-          }
-          
-        async function saveCredentials(client) {
-            const content = await fs.readFile(CREDENTIALS_PATH);
-            const keys = JSON.parse(content);
-            const key = keys.installed || keys.web;
-            const payload = JSON.stringify({
-              type: 'authorized_user',
-              client_id: key.client_id,
-              client_secret: key.client_secret,
-              refresh_token: client.credentials.refresh_token,
+    try{          
+        async function getAuthenticatedClient() {
+          return new Promise((resolve, reject) => {
+            const oAuth2Client = new OAuth2Client(
+              keys.web.client_id,
+              keys.web.client_secret,
+              keys.web.redirect_uris[0]
+            );
+        
+            const authorizeUrl = oAuth2Client.generateAuthUrl({
+              access_type: 'offline',
+              scope: 'https://www.googleapis.com/auth/gmail.readonly',
+              prompt: 'consent'
             });
-            await fs.writeFile(TOKEN_PATH, payload);
-          }
-          
-        async function authorize() {
-            let client = await loadSavedCredentialsIfExist();
-            if (client) {
-              return client;
-            }
-            client = await authenticate({
-              scopes: SCOPES,
-              keyfilePath: CREDENTIALS_PATH,
-            });
-            if (client.credentials) {
-              await saveCredentials(client);
-            }
-            return client;
-          }
+        
+            const server = http
+              .createServer(async (req, res) => {
+                try {
+                  if (req.url.indexOf('/') > -1) {
+                    const qs = new url.URL(req.url, 'http://localhost:3000')
+                      .searchParams;
+                    const code = qs.get('code');
+                    console.log(`Code is ${code}`);
+                    res.end('Authentication successful! Please return to the console.');
+                    server.destroy();
+                    const r = await oAuth2Client.getToken(code);
+                    oAuth2Client.setCredentials(r.tokens);
+                    console.info('Tokens acquired.');
+                    resolve(oAuth2Client);
+                  }
+                } catch (e) {
+                  reject(e);
+                }
+              })
+              .listen(3000, () => {
+                open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+              });
+            destroyer(server);
+          });
+        }
           
         async function listThreads() {
-            const auth = await authorize()
+            const auth = await getAuthenticatedClient()
             const gmail = google.gmail({version: 'v1', auth});
             const res = await gmail.users.threads.list({
               userId: 'me',
